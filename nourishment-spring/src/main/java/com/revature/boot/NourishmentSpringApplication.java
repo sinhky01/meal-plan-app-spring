1e6a01702775e7d60bb4721b103f69fe02981384
sinhky01 <kyle.sinhart@gmail.com>
2019-08-02 14:49:53 -0400

    set-up Spring project

---
+++ nourishment-spring/src/main/java/com/revature/boot/NourishmentSpringApplication.java
@@ -0,0 +1,13 @@
+package com.revature.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NourishmentSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NourishmentSpringApplication.class, args);
+	}
+
+}
---
+++ nourishment-spring/src/main/java/com/revature/boot/service/UserService.java
@@ -0,0 +1,5 @@
+package com.revature.boot.service;
+
+public class UserService {
+
+}
